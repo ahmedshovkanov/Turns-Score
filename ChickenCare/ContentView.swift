16797c2c8682539c30bfc2b81e376ada6a107c64
NBLWork <nikitaborchevsky@syndicate-g.com>
2025-11-06 14:53:54 +0300

    fix push screen

--- ChickenCare/ContentView.swift
+++ ChickenCare/ContentView.swift
@@ -456,11 +456,12 @@ class SplashViewModel: ObservableObject {
         
         // усли не с пуша открыли запрашиваем
         if webViewURL == nil {
-            if !UserDefaults.standard.bool(forKey: "accepted_notifications") && !UserDefaults.standard.bool(forKey: "system_close_notifications") {
-                checkAndShowNotificationScreen()
-            } else {
-                sendConfigRequest()
-            }
+            sendConfigRequest()
+//            if !UserDefaults.standard.bool(forKey: "accepted_notifications") && !UserDefaults.standard.bool(forKey: "system_close_notifications") {
+//                checkAndShowNotificationScreen()
+//            } else {
+//                
+//            }
         }
     }
     
